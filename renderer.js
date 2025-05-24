22ebdb23bee0d247dbfb2af974e826deff69821e
gatoAndres5 <andgalv@arizona.eduu>
2025-05-23 23:25:49 -0700

    ingredients

--- renderer.js
+++ renderer.js
@@ -3,9 +3,15 @@ const { ipcRenderer } = require('electron');
 
 window.addEventListener('DOMContentLoaded', () => {
   const configBtn = document.getElementById('go-to-config');
+  const homeBtn = document.getElementById('go-to-home')
   if (configBtn) {
     configBtn.addEventListener('click', () => {
       ipcRenderer.send('navigate-to-config');
+    });
+  }
+  if (homeBtn) {
+    homeBtn.addEventListener('click', () => {
+      ipcRenderer.send('navigate-to-home');
     });
   }
 });
